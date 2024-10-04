dfd4e6c75f558eab7dd845c77c39dcf2c4808a82
Shahrier Hossain <jbc.shahrier@gmail.com>
2024-10-05 04:30:10 +0600

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,8 +66,8 @@ export default function Intro() {
         <span className="font-bold">Software Engineer</span> with{" "}
         <span className="font-bold">more than 1.5 years</span> of experience. I love
         building <span className="italic">apps and sites</span>.
-        😑 amm... well, actually I want to be a <span className="font-bold italic">Researcher</span>.
-         Highly interested in <span className="font-bold italic">Neuro Data Science</span>, <span className="font-bold italic">Brain Computer Interactions</span>. and <span className="font-bold italic"> Human Behaviour & Psychology </span>.
+        😑 amm... well, actually I want to be a <span className="font-bold">Researcher</span>.
+         Highly interested in <span className="font-bold ">Neuro Data Science</span>, <span className="font-bold">Brain Computer Interactions</span>. and <span className="font-bold"> Human Behaviour & Psychology </span>.
       </motion.h1>
 
       <motion.div
