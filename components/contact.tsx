34abe55f7d62243fa90fd4d93e842f87ef57307f
Shahrier Hossain <jbc.shahrier@gmail.com>
2024-10-05 04:12:19 +0600

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="mailto:hossainshahrier.sh@gmail.com">
+          hossainshahrier.sh@gmail.com
         </a>{" "}
         or through this form.
       </p>
