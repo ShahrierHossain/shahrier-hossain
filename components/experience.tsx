94e3b612d0841196003d393d92f54c71866d727c
ShahrierHossain <jbc.shahrier@gmail.com>
2025-08-28 17:06:44 +0600

    Design  Change in Experience

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,11 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
+      <VerticalTimeline
+        lineColor={
+          theme === "light" ? "rgba(0,0,0,0.2)" : "rgba(255,255,255,0.2)"
+        }
+      >
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
